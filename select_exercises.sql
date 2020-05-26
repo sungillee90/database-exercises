ad14306a41d6c8f180e7684f6b0719f64923fd12
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-26 16:04:40 -0500

    Partially finished exercises.

---
+++ select_exercises.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+SELECT 'The name of all albums by Pink Floyd.' AS 'Exercise 1';
+
+SELECT * from albums where artist = 'Pink Floyd';
+
+SELECT release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
