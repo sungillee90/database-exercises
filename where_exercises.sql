4e51fd7868f557e5c6bda68dc923af2f5c54873d
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-27 15:54:46 -0500

    Finished Where exercise with IntelliJ setup.

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,14 +13,16 @@ SELECT *
 # Find all employees hired in the 90s — 135,214 rows.
 SELECT *
     FROM employees
-    WHERE hire_date >= '1990-01-01'
-        AND hire_date <= '1999-12-31';
+#     WHERE hire_date >= '1990-01-01'
+#     AND hire_date <= '1999-12-31';
+#                   LIKE '199%'; ALSO work
+    WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 # Find all employees born on Christmas — 842 rows.
 SELECT *
     FROM employees
-#   WHERE birth_date LIKE ('%12-25') ALSO WORKED
-    WHERE birth_date LIKE ('%12-25%');
+#   WHERE birth_date LIKE '%12-25' ALSO WORKED
+    WHERE birth_date LIKE '%12-25%';
 
 # Find all employees with a 'q' in their last name — 1,873 rows.
 SELECT *
@@ -28,3 +30,42 @@ SELECT *
 #                         %q로 끝나는 이름
 #     WHERE last_name LIKE ('%q');
     WHERE last_name LIKE ('%q%');
+
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT *
+    FROM employees
+    WHERE first_name = ('Irena')
+        OR first_name = ('Vidya')
+        OR first_name = ('Maya');
+
+# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+SELECT *
+FROM employees
+WHERE (
+    first_name = ('Irena')
+    OR first_name = ('Vidya')
+    OR first_name = ('Maya')
+    )
+    AND gender = 'M';
+
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT *
+    FROM employees
+    WHERE hire_date LIKE '199%'
+        AND birth_date LIKE '%12-25';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+    FROM employees
+    WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
