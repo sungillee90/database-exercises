7efc22d627561b16b1bf6cc68478ac6109b3bcc6
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-28 10:38:46 -0500

    Complete Limit exercise.

---
+++ limit_exercises.sql
@@ -0,0 +1,33 @@
+USE employees;
+
+# MySQL provides a way to return only unique results from our queries with the keyword DISTINCT.
+# For example, to find all the unique titles within the company, we could run the following query:
+SELECT DISTINCT title FROM titles;
+
+# List the first 10 distinct last name sorted in descending order.
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+# Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql.
+# Update it to find just the first 5 employees. Their names should be:
+SELECT *
+FROM employees
+WHERE (
+                  hire_date LIKE '199%'
+              AND birth_date LIKE '%12-25'
+          )
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
+# Try to think of your results as batches, sets, or pages. The first five results are your first page.
+# The five after that would be your second page, etc.
+# Update the query to find the tenth page of results.
+SELECT *
+FROM employees
+WHERE (
+                  hire_date LIKE '199%'
+              AND birth_date LIKE '%12-25'
+          )
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45 ;
